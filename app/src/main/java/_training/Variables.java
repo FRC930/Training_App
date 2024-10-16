dff0328aed2492e0c0260c0671acde19c8d644ee
CarOne2010 <carter.r.kane@gmail.com>
2024-10-15 20:55:55 -0500

    I followed the torule up to step 2

--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -2,11 +2,16 @@ package _training;
 
 public class Variables {
     public static void main(String[] args) {
-        int myNum = 1;
-        int myOtherNum = 2;
+        int myNum = 10;
+        int myOtherNum = 10;
+        int myOtherOtherNum = 10;
 
         // Get the sum of the two numbers
-        int myFinalNum = myNum + myOtherNum;
+        int myFinalNum = (myNum + myOtherNum)* myOtherOtherNum;
         System.out.println(myFinalNum);
+        String myText = "Hello!";
+        String myOtherText = " Again!";
+        String myFinalText = myText + myOtherText;
+        System.out.println(myFinalText);
     }
 }
