2ab94951650e0b68f0ebed5dae5ed3e16f6c0aa1
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2024-10-17 21:00:19 -0500

    Continued work on variables
    Made changes

--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -8,5 +8,26 @@ public class Variables {
         // Get the sum of the two numbers
         int myFinalNum = myNum + myOtherNum;
         System.out.println(myFinalNum);
+
+        String myText = "Hello!"; 
+        String myOtherText = " Again!";
+        String myOtherotherText = "!";
+
+        String myFinalText = myText + myOtherText + myOtherotherText;
+        System.out.println(myFinalText);
+
+        float myDecimal = 2.5f; 
+        float mySecondDecimal = 5.0f;
+        float myLastDecimal = myDecimal / mySecondDecimal;
+
+        System.out.println(myLastDecimal);
+
+        boolean ateBreakfast = true;
+        boolean ateLunch = true;
+        boolean ateDinner = false;
+
+        System.out.println(ateBreakfast && ateLunch);
+        System.out.println(ateLunch && ateDinner);
+
     }
 }
