f57399012442da1ad3590dea64c782c5e5d15c74
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2024-10-17 20:53:28 -0500

    Fixing
    Fix "While.java".

--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -5,13 +5,23 @@ public class Variables {
         int myNum = 62;
         int myOtherNum = 12;
         int myOtherOtherNum = 10;
+        float myDecimal = 2.5f;
+        float mySecondDecimal = 5.0f;
+        boolean ateBreakfast = true;
+        boolean ateLunch = true;
+        boolean ateDinner = false;
+
 
         // Get the sum of the two numbers
         int myFinalNum = (myNum + myOtherNum) * myOtherOtherNum;
+        float myFinalDecimal = myDecimal / mySecondDecimal;
         System.out.println(myFinalNum);
-    String myText = "Hello!";
-    String myOtherText = " Again";
-    String myFinalText = myText + myOtherText;
-    System.out.println(myFinalText);
+        String myText = "Hello!";
+        String myOtherText = " Again";
+        String myFinalText = myText + myOtherText;
+        System.out.println(myFinalText);
+        System.out.println(myFinalDecimal);
+        System.out.println(ateBreakfast && ateLunch);
+        System.out.println(ateLunch && ateDinner);
     }
 }
