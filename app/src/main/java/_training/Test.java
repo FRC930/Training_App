f2080ad768c5e0c29cc5d79954108cd888a15de2
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2024-10-17 19:23:32 -0500

    Update Variables.java

--- app/src/main/java/_training/Test.java
+++ app/src/main/java/_training/Test.java
@@ -5,5 +5,8 @@ public class Test {
         for (int i = 0; i < 101; i+= 2) {
             System.out.println("I ran in " + i + " circles today!");
         }
+        for (int i = 1; i < 101; i+= 2) {
+            System.out.println("I ran in " + i + " circles today!");
+        }
     }
 }
