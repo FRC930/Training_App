4b0c323a6aa21968d19f732f54cb9b84aa98fe64
CarOne2010 <carter.r.kane@gmail.com>
2024-10-17 20:54:14 -0500

    finished the readme instructions and added my own code

--- app/src/main/java/_training/IfStatement.java
+++ app/src/main/java/_training/IfStatement.java
@@ -4,11 +4,34 @@ public class IfStatement {
     public static void main(String[] args) {
         boolean brushedTeeth = true;
         boolean tookMints = false;
-        boolean didHomework = false;
+        boolean didHomework = !false;
         boolean preparedForRobotics = true;
+        boolean chargedthecomputer = !true;
+        boolean daymonday = true;
+        boolean wasfocastedinmath = true;
+
+        if ((daymonday)){
+            boolean schoolopen = true;
+        if ((schoolopen && chargedthecomputer && didHomework)){
+            System.out.println("I am going to have a good day today");
+        } else {
+            System.out.println("today is not of to a good start");
+        if ((wasfocastedinmath)){
+            boolean homeworkformath = false;
+        if ((homeworkformath)){
+            System.out.println("I was not focasted in class so I have no homework");
+            didHomework = false;
+        } else {
+            System.out.println("I was focasted in class so now I don't have homework");         
+        }   
 
         if((brushedTeeth || tookMints) && didHomework && preparedForRobotics){
             System.out.println("I can go to robotics tonight!");
+        } else { 
+            System.out.println("I can't go to robotics yet");
         }
-    }
-}
+       if((tookMints && chargedthecomputer) && didHomework){
+            System.out.println("I am going to need to borrow a computer ");
+        } else {
+            System.out.println("I have a fully charged computer. Great!");
+        }}}}}}
