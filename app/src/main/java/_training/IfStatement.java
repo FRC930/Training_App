132015a12360ca8f28b6e346362b2ed42c699dd2
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2024-10-17 20:53:28 -0500

    Fixing
    Fix "While.java".

--- app/src/main/java/_training/IfStatement.java
+++ app/src/main/java/_training/IfStatement.java
@@ -4,11 +4,17 @@ public class IfStatement {
     public static void main(String[] args) {
         boolean brushedTeeth = true;
         boolean tookMints = false;
-        boolean didHomework = false;
+        boolean didHomework = true;
         boolean preparedForRobotics = true;
 
-        if((brushedTeeth || tookMints) && didHomework && preparedForRobotics){
+        if ((brushedTeeth || tookMints) && didHomework && preparedForRobotics) {
             System.out.println("I can go to robotics tonight!");
+        } else if (!(brushedTeeth || tookMints)) {
+            System.out.println("Gotta freshen up!");
+        } else if (!didHomework) {
+            System.out.println("Oh darn, I gotta do my Math homework!");
+        } else if (!preparedForRobotics) {
+            System.out.println("Oop, I need to finish finding my sponsors!");
         }
     }
 }
