f471a594160ce75cc239b5ed52d89ab7b8a427a7
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2024-10-22 20:55:47 -0500

    initial work for "applying what you learned"
    completed addition subtraction multiplication
    need to finish division

--- app/src/main/java/_training/MathProblems.java
+++ app/src/main/java/_training/MathProblems.java
@@ -117,5 +117,93 @@ public class MathProblems {
 
         //done
         
+        //section 1 addition
+
+        int j = 1;
+        int k = 2;
+
+        int l = j + k;
+        System.out.println(l);
+
+        float q = 3.6f;
+        float w = 4.3f;
+
+        float e = q + w;
+        System.out.println(e);
+
+        double n = 9.29485798364943872864;
+        double m = 3.14159265;
+
+        double b = n + m;
+        System.out.println(b);
+
+        double lskdjf = Math.PI;
+        double owieur = Math.PI;
+
+        double ivchsdfh = lskdjf + owieur;
+        System.out.println(ivchsdfh);
+
+        //section 2 subtraction
+
+        int ab = 34;
+        int cd = 12;
+
+        int ef = ab - cd;
+        System.out.println(ef);
+
+        float gh = 56;
+        float ij = 78;
+
+        float kl =  gh - ij;
+        System.out.println(kl);
+
+        double mn = Math.PI;
+        double op = Math.PI;
+
+        double qr = mn - op;
+        System.out.println(qr);
+
+        double something = Math.PI;
+        double somethingelse = Math.PI;
+        double somethingelseelse = Math.PI;
+
+        double somethingelseelseelse = something - somethingelse - somethingelseelse;
+        System.out.println(somethingelseelseelse);
+
+        //section 3 multiplication
+
+        int who = 135;
+        int whowho = 868;
+
+        int whowhowho = who * whowho;
+        System.out.println(whowhowho);
+
+        float why = 97;
+        float whywhy = 65;
+
+        float whywhywhy = why * whywhy;
+        System.out.println(whywhywhy);
+
+        double me = -2134;
+        double meme = 654;
+
+        double mememe = me * meme;
+        System.out.println(mememe);
+
+        double ah = Math.PI;
+
+        double ahah = ah * ah;
+        System.out.println(ahah);
+
+        //section 4 division
+
+        int dime = -9;
+        int dimedimedime = 9;
+
+        int dimedime = dimedimedime / dime;
+        System.out.println(dimedime);
+
+        float quarter = 5;
+        
     }
 }
