6450e78734045bd34b151f7732303a147c727f6f
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2024-10-24 20:15:56 -0500

    Added "MathProblems.java"

--- app/src/main/java/_training/MathProblems.java
+++ app/src/main/java/_training/MathProblems.java
@@ -2,6 +2,73 @@ package _training;
 
 public class MathProblems {
     public static void main(String[] args) {
-        
+    int myNum = 1;
+    int myNum2 = 2;
+    int myMinusNum = 1;
+    int myMinusNum2 = 2;
+    int myMultiNum = 1;
+    int myMultiNum2 = 2;
+    int myDiviNum = 1;
+    int myDiviNum2 = 2;
+    float myFloatNum = 1;
+    float myFloatNum2 = 2;
+    float myMinusFloatNum = 1;
+    float myMinusFloatNum2 = 2;
+    float myMultiFloatNum = 1;
+    float myMultiFloatNum2 = 2;
+    float myDiviFloatNum = 1;
+    float myDiviFloatNum2 = 2;
+    double myDoubleNum = 1;
+    double myDoubleNum2 = 2;
+    double myMinusDoubleNum = 1;
+    double myMinusDoubleNum2 = 2;
+    double myMultiDoubleNum = 1;
+    double myMultiDoubleNum2 = 2;
+    double myDiviDoubleNum = 1;
+    double myDiviDoubleNum2 = 2; 
+
+    int myFinalNum = (myNum + myNum2);
+    int myFinalMinusNum = (myMinusNum - myMinusNum2);
+    int myFinalMultiNum = (myMultiNum * myMultiNum2);
+    int myFinalDiviNum = (myDiviNum / myDiviNum2);
+    float myFinalFloatNum = (myFloatNum + myFloatNum2);
+    float myFinalMinusFloatNum = (myMinusFloatNum - myMinusFloatNum2);
+    float myFinalMultiFloatNum = (myMultiFloatNum * myMultiFloatNum2);
+    float myFinalDiviFloatNum = (myDiviFloatNum / myDiviFloatNum2);
+    double myFinalDoubleNum = (myDoubleNum + myDoubleNum2);
+    double myFinalMinusDoubleNum = (myMinusDoubleNum - myMinusDoubleNum2);
+    double myFinalMultiDoubleNum = (myMultiDoubleNum * myMultiDoubleNum2);
+    double myFinalDiviDoubleNum = (myDiviDoubleNum / myDiviDoubleNum2);
+    
+    System.out.println ("");
+
+    System.out.println ("ADDITION");
+    System.out.println ("int = " + myFinalNum);
+    System.out.println ("Double = " + myFinalDoubleNum);
+    System.out.println ("Float = " + myFinalFloatNum);
+    
+    System.out.println ("");
+
+    System.out.println ("MINUS");
+    System.out.println ("int Minus = " + myFinalMinusNum);
+    System.out.println ("Float Minus = " + myFinalMinusFloatNum);
+    System.out.println ("Double Minus = " + myFinalMinusDoubleNum);
+    
+    System.out.println ("");
+
+    System.out.println ("MULTIPLY");
+    System.out.println ("int Multiply = " + myFinalMultiNum);
+    System.out.println ("Float Multiply = " + myFinalMultiFloatNum);
+    System.out.println ("Double Multiply = " + myFinalMultiDoubleNum);
+    
+    System.out.println ("");
+
+    System.out.println ("DIVIDE");
+    System.out.println ("int Divide = " + myFinalDiviNum);
+    System.out.println ("Float Divide = " + myFinalDiviFloatNum);
+    System.out.println ("Double Divide = " + myFinalDiviDoubleNum);
+    
+    System.out.println ("");
+
     }
 }
--- app/src/main/java/_training/While.java
+++ app/src/main/java/_training/While.java
@@ -12,5 +12,4 @@ public class While {
             System.out.println("I have worked for "+hoursWorked+" hours!");
             hoursWorked = hoursWorked + 1;
         }
-        System.out.println("I am done working! I have worked for "+hoursWorked+" hours!");
 }
