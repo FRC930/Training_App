b62b89c1da16e44bdd1756bf89d8727a73365d0b
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2024-10-24 20:51:32 -0500

    Updated Functions.java
    I updated the "Functions.java" file.

--- app/src/main/java/_training/Functions.java
+++ app/src/main/java/_training/Functions.java
@@ -3,6 +3,9 @@ package _training;
 public class Functions {
     static void printHi() {
         System.out.println("Hello!");
+        System.out.println("Hallo");
+        System.out.println("No");
+        System.out.println("Nein");
     }
 
     public static void main(String[] args) {
