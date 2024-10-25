8b4028df85bdb9fca1bfc1460e85793ab5c45f1e
bodenaarhus <boden7607@gmail.com>
2024-10-24 20:52:21 -0500

    initial traning
    i did training with variables, if statements, while loops and started the math problems.

--- app/src/main/java/_training/MathProblems.java
+++ app/src/main/java/_training/MathProblems.java
@@ -51,37 +51,16 @@ public class MathProblems {
     
     public static void main(String[] args) {
         
-       
-            System.out.println(addition(5, 2));
+        int myNum = 1;
+        int myotherNum = 2;
+        int myFinalNum = myNum + myotherNum;
+        System.out.println(myFinalNum);
 
-            System.out.println(addition(4, 1));
+        float mydecimal = 3.0f;
+        float mydecimal2 = 4.0f;
+        float myFinalDecimal = mydecimal + mydecimal2; 
+        System.out.println(myFinalDecimal);
 
-            System.out.println(addition(3, 0));
-
-            System.out.println(additions(2, 6));
-
-            System.out.println(subtraction(3, 4));
-
-            System.out.println(subtraction(2, 3));
-
-            System.out.println(subtraction(1, 2));
-
-            System.out.println(subtraction(0, 1));
-
-            System.out.println(multiplacation(1, 6));
-
-            System.out.println(multiplacation(4, 2));
-
-            System.out.println(multiplacation(5,3));
-
-            System.out.println(multiplacations(5,4));
-
-            System.out.println(divide(8, 2));
-
-            System.out.println(divide(7, 1));
-
-            System.out.println(divide(6, 0));
-
-            System.out.println(divides(5, 3));
+        double my
     }
 }
