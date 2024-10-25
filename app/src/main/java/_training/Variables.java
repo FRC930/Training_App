eb3408b6e60c0d4a675d73b6e50b4cc84f482fdb
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2024-10-24 20:25:48 -0500

    Cleaned up Variables.java

--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -5,18 +5,12 @@ public class Variables {
         int myNum = 62;
         int myOtherNum = 12;
         int myOtherOtherNum = 10;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8fd0384 (Fixing)
         float myDecimal = 2.5f;
         float mySecondDecimal = 5.0f;
         boolean ateBreakfast = true;
         boolean ateLunch = true;
         boolean ateDinner = false;
 
-<<<<<<< HEAD
-
         // Get the sum of the two numbers
         int myFinalNum = (myNum + myOtherNum) * myOtherOtherNum;
         float myFinalDecimal = myDecimal / mySecondDecimal;
@@ -28,28 +22,5 @@ public class Variables {
         System.out.println(myFinalDecimal);
         System.out.println(ateBreakfast && ateLunch);
         System.out.println(ateLunch && ateDinner);
-=======
-=======
->>>>>>> 8fd0384 (Fixing)
-
-        // Get the sum of the two numbers
-        int myFinalNum = (myNum + myOtherNum) * myOtherOtherNum;
-        float myFinalDecimal = myDecimal / mySecondDecimal;
-        System.out.println(myFinalNum);
-<<<<<<< HEAD
-    String myText = "Hello!";
-    String myOtherText = " Again";
-    String myFinalText = myText + myOtherText;
-    System.out.println(myFinalText);
->>>>>>> e6beb18 (Update Variables.java)
-=======
-        String myText = "Hello!";
-        String myOtherText = " Again";
-        String myFinalText = myText + myOtherText;
-        System.out.println(myFinalText);
-        System.out.println(myFinalDecimal);
-        System.out.println(ateBreakfast && ateLunch);
-        System.out.println(ateLunch && ateDinner);
->>>>>>> 8fd0384 (Fixing)
     }
 }
