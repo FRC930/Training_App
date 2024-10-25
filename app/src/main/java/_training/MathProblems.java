2417153b6213b075b1097f29432a53512a02f121
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2024-10-24 20:16:09 -0500

    finished applying
    made the terminal output nicer

--- app/src/main/java/_training/MathProblems.java
+++ app/src/main/java/_training/MathProblems.java
@@ -118,92 +118,125 @@ public class MathProblems {
         //done
         
         //section 1 addition
+        System.out.println("\n");
+        System.out.println("Addition");
+        System.out.println("\n");
 
         int j = 1;
         int k = 2;
 
         int l = j + k;
-        System.out.println(l);
+        System.out.println(l + " int");
 
         float q = 3.6f;
         float w = 4.3f;
 
         float e = q + w;
-        System.out.println(e);
+        System.out.println(e + " float");
 
         double n = 9.29485798364943872864;
         double m = 3.14159265;
 
         double b = n + m;
-        System.out.println(b);
+        System.out.println(b + " double");
+
 
         double lskdjf = Math.PI;
         double owieur = Math.PI;
 
         double ivchsdfh = lskdjf + owieur;
-        System.out.println(ivchsdfh);
+        System.out.println(ivchsdfh + " my problem");
+        
+        System.out.println("\n");
+        System.out.println("Subtraction");
+        System.out.println("\n");
 
         //section 2 subtraction
+
 
         int ab = 34;
         int cd = 12;
 
         int ef = ab - cd;
-        System.out.println(ef);
+        System.out.println(ef + " int");
 
         float gh = 56;
         float ij = 78;
 
         float kl =  gh - ij;
-        System.out.println(kl);
+        System.out.println(kl + " float");
 
         double mn = Math.PI;
         double op = Math.PI;
 
         double qr = mn - op;
-        System.out.println(qr);
+        System.out.println(qr + " double");
 
         double something = Math.PI;
         double somethingelse = Math.PI;
         double somethingelseelse = Math.PI;
 
         double somethingelseelseelse = something - somethingelse - somethingelseelse;
-        System.out.println(somethingelseelseelse);
+        System.out.println(somethingelseelseelse + " my problem");
 
         //section 3 multiplication
+        System.out.println("\n");
+        System.out.println("Multiplication");
+        System.out.println("\n");
 
         int who = 135;
         int whowho = 868;
 
         int whowhowho = who * whowho;
-        System.out.println(whowhowho);
+        System.out.println(whowhowho + " int");
 
         float why = 97;
         float whywhy = 65;
 
         float whywhywhy = why * whywhy;
-        System.out.println(whywhywhy);
+        System.out.println(whywhywhy + " float");
 
         double me = -2134;
         double meme = 654;
 
         double mememe = me * meme;
-        System.out.println(mememe);
+        System.out.println(mememe + " double");
 
         double ah = Math.PI;
 
         double ahah = ah * ah;
-        System.out.println(ahah);
+        System.out.println(ahah + " my problem");
 
         //section 4 division
+        System.out.println("\n");
+        System.out.println("Division");
+        System.out.println("\n");
 
         int dime = -9;
         int dimedimedime = 9;
 
         int dimedime = dimedimedime / dime;
-        System.out.println(dimedime);
+        System.out.println(dimedime + " int");
 
-        float quarter = 5;
+        float quarter = 5.2f;
+        float quarterquarter = -89.7f;
+
+        float quarterquarterquarter = quarter / quarterquarter;
+        System.out.println(quarterquarterquarter + " float");
+
+        double nickle = 3.14159634785672356;
+        double nicklenickle = -957939279;
+
+        double nicklenicklenickle = nickle / nicklenickle;
+        System.out.println(nicklenicklenickle + " double");
+
+        int last = 1;
+        int lastproblem = 89;
+
+        int finalvariable = last + lastproblem * last;
+        System.out.println(finalvariable + " my last problem");
+
+        //done
         
     }
 }
