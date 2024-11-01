24e67cd1bc3a39bfdfd4f5e5522d6dde9d294201
WasteOfTheOcean <96154793+WasteOfTheOcean@users.noreply.github.com>
2024-10-31 19:12:27 -0500

    variable changes

--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -4,9 +4,16 @@ public class Variables {
     public static void main(String[] args) {
         int myNum = 1;
         int myOtherNum = 2;
+        int myNum3 = 3;
+
+        double doubleNumber = 1.6;
+        double otherDoubleNumber = 1.3;
+
+        int total = (int) (doubleNumber + otherDoubleNumber);
+        System.out.println(total);
 
         // Get the sum of the two numbers
-        int myFinalNum = myNum + myOtherNum;
+        int myFinalNum = myNum3 * (myNum + myOtherNum);
         System.out.println(myFinalNum);
     }
 }
