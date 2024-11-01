d2a3186d833010f12b1fc7a603d4e2b0ee6c414a
bodenaarhus <boden7607@gmail.com>
2024-10-31 19:53:09 -0500

    update
    i did equations

--- app/src/main/java/_training/MathProblems.java
+++ app/src/main/java/_training/MathProblems.java
@@ -61,6 +61,54 @@ public class MathProblems {
         float myFinalDecimal = mydecimal + mydecimal2; 
         System.out.println(myFinalDecimal);
 
-        double my
+        double mydecimal3 = 5.00;
+        double mydecimal4 = 6.00;
+        double myfinaldecimal2 = mydecimal3 + mydecimal4;
+        System.out.println(myfinaldecimal2);
+
+        int mynum2 = 2;
+        int myothernum2 = 3; 
+        int myFinalNum2 = mynum2 - myothernum2;
+        System.out.println(myFinalNum2);
+
+        float mydecimal5 = 4.0f;
+        float mydecimal6 = 5.0f;
+        float myfinaldecimal3 = mydecimal5 - mydecimal6;
+        System.out.println(myfinaldecimal3);
+
+        double mydecimal7 = 6.00;
+        double mydecimal8 = 7.00; 
+        double myfinaldecimal4 = mydecimal7 - mydecimal8;
+        System.out.println(myfinaldecimal4);
+
+        int mynum3 = 3;
+        int myothernum3 = 4;
+        int myfinalnum3 = mynum3 * myothernum3; 
+        System.out.println(myfinalnum3);
+
+        float mydecimal9 = 7.0f; 
+        float mydecimal10 = 8.0f;
+        float myfinaldecimal5 = mydecimal9 * mydecimal10;
+        System.out.println(myfinaldecimal5);
+
+        double mydecimal11 = 9.00;
+        double mydecimal12 = 10.00;
+        double myfinaldecimal6 = mydecimal11 * mydecimal12;
+        System.out.println(myfinaldecimal6);
+
+        int mynum4 = 5;
+        int myothernum4 = 6;
+        int myfinalnum4 = mynum4 / myothernum4;
+        System.out.println(myfinalnum4);
+
+        float mydecimal13 = 11.0f;
+        float mydecimal14 = 12.0f; 
+        float myfinaldecimal7 = mydecimal13 / mydecimal14;
+        System.out.println(myfinaldecimal7);
+
+        double mydecimal15 = 15.00;
+        double mydecimal16 = 16.00;
+        double myfinaldecimal8 = mydecimal15 / mydecimal16;
+        System.out.println(myfinaldecimal8);
     }
 }
