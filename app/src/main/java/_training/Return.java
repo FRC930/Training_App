85e2f7a0b145f90c40c3a5ae7a582dfc87c97fa9
bodenaarhus <boden7607@gmail.com>
2024-10-31 20:35:40 -0500

    return and perameters
    did returns and parameters

--- app/src/main/java/_training/Return.java
+++ app/src/main/java/_training/Return.java
@@ -9,11 +9,13 @@ public class Return {
         float result = num/num;
         return result;}
     public static void main(String[] args){
-        
+        float squareFive = square(5);
+        float squareTwo = square(2);
         System.out.println(square(5) + square(2));
 
+        float squaresix = square(6);
+        float squarethree = square(3);
         System.out.println(square(5) + square(3));
 
-        System.out.println(square1(5) / square1(2));
     }
 }
