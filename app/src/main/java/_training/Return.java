1669713b69aca123d22f6f1a7d695a848d8d1047
bodenaarhus <boden7607@gmail.com>
2024-11-05 20:35:26 -0600

    updated training
    11/5/2024 simple math problems

--- app/src/main/java/_training/Return.java
+++ app/src/main/java/_training/Return.java
@@ -1,14 +1,19 @@
 package _training;
 
 public class Return {
-    static float square(float num){
-        return num*num;
+    static int square(int num){
+        int result = num*num;
+        return result;
     }
+    static float square1(float num){
+        float result = num/num;
+        return result;}
     public static void main(String[] args){
         
         System.out.println(square(5) + square(2));
 
         System.out.println(square(5) + square(3));
 
+        System.out.println(square1(5) / square1(2));
     }
 }
