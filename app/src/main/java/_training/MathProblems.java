3f285bd4ea415c0dee3b04231a8ecceac89d5165
bodenaarhus <boden7607@gmail.com>
2024-11-05 20:35:26 -0600

    updated training
    11/5/2024 simple math problems

--- app/src/main/java/_training/MathProblems.java
+++ app/src/main/java/_training/MathProblems.java
@@ -54,55 +54,34 @@ public class MathProblems {
        
             System.out.println(addition(5, 2));
 
+            System.out.println(addition(4, 1));
+
+            System.out.println(addition(3, 0));
+
+            System.out.println(additions(2, 6));
+
             System.out.println(subtraction(3, 4));
 
-        
+            System.out.println(subtraction(2, 3));
 
-        
+            System.out.println(subtraction(1, 2));
 
-        int mynum2 = 2;
-        int myothernum2 = 3; 
-        int myFinalNum2 = mynum2 - myothernum2;
-        System.out.println(myFinalNum2);
+            System.out.println(subtraction(0, 1));
 
-        float mydecimal5 = 4.0f;
-        float mydecimal6 = 5.0f;
-        float myfinaldecimal3 = mydecimal5 - mydecimal6;
-        System.out.println(myfinaldecimal3);
+            System.out.println(multiplacation(1, 6));
 
-        double mydecimal7 = 6.00;
-        double mydecimal8 = 7.00; 
-        double myfinaldecimal4 = mydecimal7 - mydecimal8;
-        System.out.println(myfinaldecimal4);
+            System.out.println(multiplacation(4, 2));
 
-        int mynum3 = 3;
-        int myothernum3 = 4;
-        int myfinalnum3 = mynum3 * myothernum3; 
-        System.out.println(myfinalnum3);
+            System.out.println(multiplacation(5,3));
 
-        float mydecimal9 = 7.0f; 
-        float mydecimal10 = 8.0f;
-        float myfinaldecimal5 = mydecimal9 * mydecimal10;
-        System.out.println(myfinaldecimal5);
+            System.out.println(multiplacations(5,4));
 
-        double mydecimal11 = 9.00;
-        double mydecimal12 = 10.00;
-        double myfinaldecimal6 = mydecimal11 * mydecimal12;
-        System.out.println(myfinaldecimal6);
+            System.out.println(divide(8, 2));
 
-        int mynum4 = 5;
-        int myothernum4 = 6;
-        int myfinalnum4 = mynum4 / myothernum4;
-        System.out.println(myfinalnum4);
+            System.out.println(divide(7, 1));
 
-        float mydecimal13 = 11.0f;
-        float mydecimal14 = 12.0f; 
-        float myfinaldecimal7 = mydecimal13 / mydecimal14;
-        System.out.println(myfinaldecimal7);
+            System.out.println(divide(6, 0));
 
-        double mydecimal15 = 15.00;
-        double mydecimal16 = 16.00;
-        double myfinaldecimal8 = mydecimal15 / mydecimal16;
-        System.out.println(myfinaldecimal8);
+            System.out.println(divides(5, 3));
     }
 }
