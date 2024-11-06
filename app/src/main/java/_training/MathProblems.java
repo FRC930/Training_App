1d617ced4575da75764c75187ed78f5cb87072bc
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2024-11-05 20:25:12 -0600

    Updated Training
    On 11/5/2024

--- app/src/main/java/_training/MathProblems.java
+++ app/src/main/java/_training/MathProblems.java
@@ -1,41 +1,53 @@
 package _training;
 
 public class MathProblems {
-    static void addInt(int myNum, int myNum2){
+    static int addInt(int myNum, int myNum2){
         System.out.println(myNum+myNum2);
+        return myNum + myNum2;
     }
-    static void subInt(int myMinusNum, int myMinusNum2){
+    static int subInt(int myMinusNum, int myMinusNum2){
         System.out.println(myMinusNum+myMinusNum2);
+        return myMinusNum - myMinusNum2;
     }
-    static void multiInt(int myMultiNum, int myMultiNum2){
+    static int multiInt(int myMultiNum, int myMultiNum2){
         System.out.println(myMultiNum+myMultiNum2);
+        return myMultiNum * myMultiNum2;
     }
-    static void diviInt(int myDiviNum, int myDiviNum2){
+    static int diviInt(int myDiviNum, int myDiviNum2){
         System.out.println(myDiviNum+myDiviNum2);
+        return myDiviNum / myDiviNum2;
     }
-    static void addFloat(float myFloatNum, float myFloatNum2){
+    static float addFloat(float myFloatNum, float myFloatNum2){
         System.out.println(myFloatNum+myFloatNum2);
+        return myFloatNum + myFloatNum2;
     }
-    static void subFloat(float myMinusFloatNum, int myMinusFloatNum2){
+    static float subFloat(float myMinusFloatNum, int myMinusFloatNum2){
         System.out.println(myMinusFloatNum+myMinusFloatNum2);
+        return myMinusFloatNum - myMinusFloatNum2;
     }
-    static void multiFloat(float myMultiFloatNum, float myMultiFloatNum2){
+    static float multiFloat(float myMultiFloatNum, float myMultiFloatNum2){
         System.out.println(myMultiFloatNum+myMultiFloatNum2);
+        return myMultiFloatNum * myMultiFloatNum2;
     }
-    static void diviFloat(float myDiviFloatNum, float myDiviFloatNum2){
+    static float diviFloat(float myDiviFloatNum, float myDiviFloatNum2){
         System.out.println(myDiviFloatNum+myDiviFloatNum2);
+        return myDiviFloatNum / myDiviFloatNum2;
     }
-    static void addDouble(double myDoubleNum, double myDoubleNum2){
+    static double addDouble(double myDoubleNum, double myDoubleNum2){
         System.out.println(myDoubleNum+myDoubleNum2);
+        return myDoubleNum + myDoubleNum2;
     }
-    static void subDouble(double myMinusDoubleNum, double myMinusDoubleNum2){
+    static double subDouble(double myMinusDoubleNum, double myMinusDoubleNum2){
         System.out.println(myMinusDoubleNum+myMinusDoubleNum2);
+        return myMinusDoubleNum - myMinusDoubleNum2;
     }
-    static void multiDouble(double myMultiDoubleNum, double myMultiDoubleNum2){
+    static double multiDouble(double myMultiDoubleNum, double myMultiDoubleNum2){
         System.out.println(myMultiDoubleNum+myMultiDoubleNum2);
+        return myMultiDoubleNum * myMultiDoubleNum2;
     }
-    static void diviDouble(double myDiviDoubleNum, double myDiviDoubleNum2){
+    static double diviDouble(double myDiviDoubleNum, double myDiviDoubleNum2){
         System.out.println(myDiviDoubleNum+myDiviDoubleNum2);
+        return myDiviDoubleNum / myDiviDoubleNum2;
     }
     public static void main(String[] args){
     addInt(1,2);
@@ -54,30 +66,30 @@ public class MathProblems {
     System.out.println ("");
 
     System.out.println ("ADDITION");
-    System.out.println ("int = " + myFinalNum);
-    System.out.println ("Double = " + myFinalDoubleNum);
-    System.out.println ("Float = " + myFinalFloatNum);
+    System.out.println ("int = " + addInt(1,2));
+    System.out.println ("Double = " + addDouble(1,2));
+    System.out.println ("Float = " + addFloat(1,2));
     
     System.out.println ("");
 
     System.out.println ("MINUS");
-    System.out.println ("int Minus = " + myFinalMinusNum);
-    System.out.println ("Float Minus = " + myFinalMinusFloatNum);
-    System.out.println ("Double Minus = " + myFinalMinusDoubleNum);
+    System.out.println ("int Minus = " + subInt(1,2));
+    System.out.println ("Float Minus = " + subFloat(1,2));
+    System.out.println ("Double Minus = " + subDouble(1,2));
     
     System.out.println ("");
 
     System.out.println ("MULTIPLY");
-    System.out.println ("int Multiply = " + myFinalMultiNum);
-    System.out.println ("Float Multiply = " + myFinalMultiFloatNum);
-    System.out.println ("Double Multiply = " + myFinalMultiDoubleNum);
+    System.out.println ("int Multiply = " + multiInt(1,2));
+    System.out.println ("Float Multiply = " + multiFloat(1,2));
+    System.out.println ("Double Multiply = " + multiDouble(1,2));
     
     System.out.println ("");
 
     System.out.println ("DIVIDE");
-    System.out.println ("int Divide = " + myFinalDiviNum);
-    System.out.println ("Float Divide = " + myFinalDiviFloatNum);
-    System.out.println ("Double Divide = " + myFinalDiviDoubleNum);
+    System.out.println ("int Divide = " + diviInt(1,2));
+    System.out.println ("Float Divide = " + diviFloat(1,2));
+    System.out.println ("Double Divide = " + diviDouble(1,2));
     
     System.out.println ("");
     }
