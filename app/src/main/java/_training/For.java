1e58b66ff86835288dd77e936b17f23b26ffd669
bodenaarhus <boden7607@gmail.com>
2024-11-07 20:49:22 -0600

    for loops
    for loop training

--- app/src/main/java/_training/For.java
+++ app/src/main/java/_training/For.java
@@ -2,8 +2,29 @@ package _training;
 
 public class For {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 4; i > 0; i--) {
             System.out.println("I ran in " + i + " circles today!");
+        
+        
         }
-    }
+        for (int i = 1; i < 5; i++) {
+            System.out.println("I ran in " + i + " straight lines today!");
+        }
+        for (int i = 4; i > 0; i--) {
+            System.out.println("I ran " + i + " today!");
+        }
+        for (int i = 50; i > 0; i --) {
+            System.out.println("I ran " + i + " today!");
+        }
+
+        int i = 0;
+        while(i < 50){
+        i++;  
+            System.out.println("I ran alot " + i + " today!");
+
+        
+        }
+    
+    }        
+    
 }
---
+++ app/src/main/java/_training/test.java
@@ -0,0 +1,12 @@
+package _training;
+
+public class test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i+= 2) {
+            System.out.println("I ran in " + i + " circles today!");
+        }
+        for (int i = 1; i < 100; i+= 2) {
+            System.out.println("I ran in " + i + " circles today!");
+        }
+    }
+}
