4a4ab33bf2d56b871429d8a9154368e71fc81dfb
Liam-Rohrer <powercat201@icloud.com>
2024-11-07 20:48:24 -0600

    printing odd numbers
    done printing evens now printing odds

--- app/src/main/java/_training/For.java
+++ app/src/main/java/_training/For.java
@@ -2,8 +2,16 @@ package _training;
 
 public class For {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i > -90; i--) {
             System.out.println("I ran in " + i + " circles today!");
+        
+        
+        }
+        for (int j = 0; j < 5; j++) {
+            j = 10;
+            System.out.println( + (j +5)+" quarter circles");
+
         }
     }
+
 }
---
+++ app/src/main/java/_training/printforloops.java
@@ -0,0 +1,13 @@
+package _training;
+
+public class printforloops {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 100; i += 2) {
+            //i is equal to 0. then it will add 2 and if the number that it gets is less then 100 it will add 2 again and keep going till it hits 100
+            System.out.print(i + " ");
+            // the " " adds a space inbetween the numbers so its not all smashed to gether
+        }
+        
+    }
+}
