ec1aa83d9b1bfa4d12cd30e6c0c05f07db34aa5f
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2024-11-07 20:48:46 -0600

    Update

--- app/src/main/java/_training/For.java
+++ app/src/main/java/_training/For.java
@@ -2,8 +2,22 @@ package _training;
 
 public class For {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 4; i > 0; i--) {
             System.out.println("I ran in " + i + " circles today!");
+        }
+        for (int i = 3; i < 100; i++) {
+            System.out.println("I ran this code " + i + " times.");
+        }
+        for (int i = 100; i > 30; i--) {
+            System.out.println("I have " + i + " minutes left.");
+        }
+        for (int i = 0; i < 21; i++) {
+            System.out.println("I have " + i + " minutes left.");
+        }
+        int i = 0;
+        while(i < 20){
+        i++;
+            System.out.println("I have " + i + " minutes left.");
         }
     }
 }
