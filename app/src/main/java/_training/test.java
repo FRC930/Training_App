28d3eaf2e892cf5dc48e14eac5200db0aa7286b0
bodenaarhus <boden7607@gmail.com>
2024-11-12 20:34:08 -0600

    the even and odd print outs
    i tweaked my code for printing out the odd and even numbers

--- app/src/main/java/_training/test.java
+++ app/src/main/java/_training/test.java
@@ -8,5 +8,14 @@ public class test {
         for (int i = 1; i < 100; i+= 2) {
             System.out.println("I ran in " + i + " circles today!");
         }
+        for (int i = 1; i <=100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("even"+i);
+                //even
+            } else {
+                System.out.println("odd"+i);
+                //odd
+            }
+        }
     }
 }
