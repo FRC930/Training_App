c4ffde7259341e9a910754deb99142a14e277a69
Liam-Rohrer <powercat201@icloud.com>
2024-11-12 20:35:08 -0600

    creating a odd or even loop with mod
    mod is, a number divided by 2

--- app/src/main/java/_training/For.java
+++ app/src/main/java/_training/For.java
@@ -4,12 +4,12 @@ public class For {
     public static void main(String[] args) {
         for (int i = 0; i > -90; i--) {
             System.out.println("I ran in " + i + " circles today!");
-        
-        
-        }
-        for (int j = 0; j < 5; j++) {
-            j = 10;
-            System.out.println( + (j +5)+" quarter circles");
+
+            for (int j = 0; j < 5; j++) {
+                j = 10;
+                System.out.println(+(j + 5) + " quarter circles");
+
+            }
 
         }
     }
--- app/src/main/java/_training/printforloops.java
+++ app/src/main/java/_training/printforloops.java
@@ -8,6 +8,21 @@ public class printforloops {
             System.out.print(i + " ");
             // the " " adds a space inbetween the numbers so its not all smashed to gether
         }
+
+
+
+
         
+        boolean f = !true; //creates a flag
+        for (int i = 0; i <=100; i++){ // adds one intill i reaches 100
+            //even
+            if ((f) && ((i % 2) == 0)) { // if f is true then print even
+                System.out.println(i + " ");
+            }
+            //odd
+                else if (!f && ((i % 2) == 1)){ // if f is not true then prints odd
+                System.out.println(i + " ");// prints the number
+            }
+        }   // % is a mod, wich dividing.
     }
 }
