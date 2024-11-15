2ab09d9cbfde8510782ee507f5ba1a4e12c78466
Liam-Rohrer <powercat201@icloud.com>
2024-11-14 19:15:11 -0600

    exclaimation mark
    same thing as last time

--- app/src/main/java/_training/printforloops.java
+++ app/src/main/java/_training/printforloops.java
@@ -12,17 +12,20 @@ public class printforloops {
 
 
 
-        
-        boolean f = !true; //creates a flag
-        for (int i = 0; i <=100; i++){ // adds one intill i reaches 100
-            //even
-            if ((f) && ((i % 2) == 0)) { // if f is true then print even
+
+        boolean f = true; //Creates a flag.
+        for (int i = 0; i <=100; i++){ // Adds one untill i reaches 100.
+            //Even.
+            
+            // % is a mod, wich is dividing and returning the remander.
+            // When finding a odd or even you mod by 2. if the answer is equal to 0 then it's even, if not odd.
+            if ((f) && ((i % 2) == 0)) { // If f is true, then print even.
                 System.out.println(i + " ");
             }
-            //odd
-                else if (!f && ((i % 2) == 1)){ // if f is not true then prints odd
-                System.out.println(i + " ");// prints the number
+            //Odd.
+                else if (!f && ((i % 2) == 1)){ // If f is not true then prints odd.
+                System.out.println(i + " ");// Prints the number.
             }
-        }   // % is a mod, wich dividing.
-    }
-}
+        }    
+    }       
+}           
