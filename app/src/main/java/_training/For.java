7098774468c634aa72877dfe7fb2208a913d0f55
CarOne2010 <carter.r.kane@gmail.com>
2024-11-19 17:19:19 -0600

    update for for.java
    i followed the read me and finished for loops

--- app/src/main/java/_training/For.java
+++ app/src/main/java/_training/For.java
@@ -2,8 +2,11 @@ package _training;
 
 public class For {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i--) {
+        for (int i = 20; i > 0; i--)  {
             System.out.println("I ran in " + i + " circles today!");
+                for (int quarters = 4; quarters > 0; quarters--) {
+                System.out.println("I have ran in " + quarters + " quarters of the " + i +" circle!");
+            }
         }
     }
 }
