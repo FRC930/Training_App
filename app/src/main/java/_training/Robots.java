b5a3ff80d8af5f76ce1cacfb7ce3accc42099980
CarOne2010 <carter.r.kane@gmail.com>
2024-11-19 19:53:02 -0600

    updated robot.java
    I started and finished robot.java

--- app/src/main/java/_training/Robots.java
+++ app/src/main/java/_training/Robots.java
@@ -7,9 +7,22 @@ public class Robots {
     static void shootRobot(String direction){
         System.out.println("Robot is shooting to the "+direction);
     }
+    static void moveshoot(String direction, int speed){
+        System.out.println("the robot is moving at "+speed+" MPH and while facing "+direction);
+    }
+    static void jumprobot(String direction, int speed){
+        System.out.println("the robot went "+direction+" at "+speed+" MPH ");
+    }
     public static void main(String[] args){
         int robotSpeed = 20; // Set the robot speed
+        moveshoot("right", robotSpeed);
         driveRobot("forward", robotSpeed); // Drive the robot forward
         shootRobot("left"); // Shoot the robot to the left
+        int robotSpeed1 = 45; // Set the robot speed
+        moveshoot("right", robotSpeed1);
+        driveRobot("forward", robotSpeed1); // Drive the robot forward
+        shootRobot("left"); // Shoot the robot to the left
+        jumprobot("right", robotSpeed1);
     }
 }
+    
