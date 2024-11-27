1cbdf56db9f907ee921bb43a71abaeb50c57252e
bodenaarhus <boden7607@gmail.com>
2024-11-26 19:10:42 -0600

    Update test.java

--- app/src/main/java/_training/test.java
+++ app/src/main/java/_training/test.java
@@ -8,13 +8,25 @@ public class test {
         for (int i = 1; i < 100; i+= 2) {
             System.out.println("I ran in " + i + " circles today!");
         }
+
+        int odd = 1;
         for (int i = 1; i <=100; i++) {
-            if (i % 2 == 0) {
-                System.out.println("even"+i);
-                //even
-            } else {
-                System.out.println("odd"+i);
-                //odd
+            //if odd is equal 1 then it would run this code 
+            if (odd == 1) {
+                //(modular)divides i by 2 and if remandier is 1 it would be odd
+                if (i % 2 ==1){
+                    //prints out all odd numbers if there is a remandir of 1 
+                    System.out.println("odd"+i);  
+                
+                }
+            }
+            // otherwise print for even
+            else{
+                //(modular)divides i by 2 and if remandier is 0 then would be even  
+                if (i % 2 == 0) {
+                    //if the remadier is 0 then print out all even numbers
+                    System.out.println("even"+i);
+                }
             }
         }
     }
