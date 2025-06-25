2580cca5683a966bf48ba7fc81b2c30325c29d52
Nolan Buschmann <nolanbuschmann@gmail.com>
2025-06-24 19:28:19 -0500

    learned about variable types

--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -4,9 +4,26 @@ public class Variables {
     public static void main(String[] args) {
         int myNum = 1;
         int myOtherNum = 2;
-
+        int mychange = 5;
         // Get the sum of the two numbers
-        int myFinalNum = myNum + myOtherNum;
+        int myFinalNum = (myNum + myOtherNum)*mychange;
         System.out.println(myFinalNum);
+       // String
+        String myText = "Hello! ";
+        String myOtherText = "Again!";
+        String myFinalText = myText + myOtherText;
+        System.out.println(myFinalText);
+        // Float
+        float myDecimal = 2.5f;
+        float mySecondDecimal = 5.0f;
+        float myLastDecimal = myDecimal / mySecondDecimal;
+        System.out.println(myLastDecimal);
+        // Bool
+        boolean ateBreakfast = true;
+        boolean ateLunch = true;
+        boolean ateDinner = false;
+        System.out.println(ateBreakfast && ateLunch);
+        System.out.println(ateLunch && ateDinner);
     }
 }
+   
