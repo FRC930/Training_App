759bfb0b8631eaa286e04fc71622d1eabf84f978
Nolan Buschmann <nolanbuschmann@gmail.com>
2025-06-24 20:47:46 -0500

    made it so it runs in four quarters of a circle then runs next circle.

--- app/src/main/java/_training/For.java
+++ app/src/main/java/_training/For.java
@@ -2,8 +2,11 @@ package _training;
 
 public class For {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
-            System.out.println("I ran in " + i + " circles today!");
+        for (int i = 0; i < 20; i++) {     
+        for ( int quarters = 0; quarters < 5; quarters++){
+        System.out.println("I ran in " + quarters + "quarters with " + i + "circle remaining");
         }
+    System.out.println("I have to run "+i+"more circles today" );
     }
+}
 }
