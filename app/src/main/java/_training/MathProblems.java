54ef0115a185cf0f5cdc5399c45a1c2e82c44670
austinL28 <austinledin28@icloud.com>
2025-06-26 20:25:39 -0500

    Finished MathProblems

--- app/src/main/java/_training/MathProblems.java
+++ app/src/main/java/_training/MathProblems.java
@@ -2,6 +2,104 @@ package _training;
 
 public class MathProblems {
     public static void main(String[] args) {
+        //Addition Equations
+        int addint1 = 6;
+        int addint2 = 13;
+        int addint3 = addint1 + addint2;
+
+        System.out.println(addint3);
+
+        float addfloat1 = 6.4f;
+        float addfloat2 = 3.7f;
+        float addfloat3 = addfloat1 + addfloat2;
+
+        System.out.println(addfloat3);
+
+        double adddouble1 = 2.678;
+        double adddouble2 = 3.170;
+        double adddoulbe3 = adddouble1 + adddouble2;
+
+        System.out.println(adddoulbe3);
+
+        double double1 = 3.14;
+        double double2 = 2.01;
+        double doulbe3 = double1 + double2;
+
+        System.out.println(doulbe3);
+
+        //Subtraction
+        int subint1 = 12;
+        int subint2 = 6;
+        int subint3 = subint1 - subint2;
+
+        System.out.println(subint3);
+
+        float subfloat1 = 14.4f;
+        float subfloat2 = 7.2f;
+        float subfloat3 = subfloat1 - subfloat2;
+
+        System.out.println(subfloat3);
+
+        double subdouble1 = 5.7346;
+        double subdouble2 = 4.9164;
+        double subdouble3 = subdouble1 - subdouble2;
+
+        System.out.println(subdouble3);
+
+        float float1 = 6.8f;
+        float float2 = 3.4f;
+        float float3 = float1 - float2;
+
+        System.out.println(float3);
+
+        //Multiplication
+        int multint1 = 3;
+        int multint2 = 3;
+        int multint3 = multint1 * multint2;
+
+        System.out.println(multint3);
+
+        float multfloat1 = 5.2f;
+        float multlfoat2 = 2.7f;
+        float multfloat3 = multfloat1 * multlfoat2;
+
+        System.out.println(multfloat3);
+
+        double multdouble1 = 7.928;
+        double multdouble2 = 3.296;
+        double multdouble3 = multdouble1 * multdouble2;
+
+        System.out.println(multdouble3);
+
+        double pi = 3.14;
+        double radius = 2.0;
+        double area = (radius * radius) * pi;
+
+        System.out.println(area);
+
+        //Division
+        int divint1 = 6;
+        int divint2 = 2;
+        int divint3 = divint1 / divint2;
+
+        System.out.println(divint3);
+
+        float divfloat1 = 3.3f;
+        float divfloat2 = 1.1f;
+        float divfloat3 = divfloat1 / divfloat2;
         
+        System.out.println(divfloat3);
+
+        double divdouble1 = 4.444;
+        double divdouble2 = 2.222;
+        double divdouble3 = divdouble1 / divdouble2;
+
+        System.out.println(divdouble3);
+
+        int int11 = 7000;
+        int int22 = 2;
+        int int33 = int11 / int22;
+
+        System.out.println(int33);
     }
 }
