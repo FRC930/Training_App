80dec9d036c73d69730e7e0ec1fe263bc5ed9ac1
Luke <breakoutboy99@gmail.com>
2025-09-30 20:48:31 -0700

    Add computer file

---
+++ app/src/main/java/_training/Classes/Computer.java
@@ -0,0 +1,16 @@
+package _training.Classes;
+
+public class Computer {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+
+    // TODO: Add multiplication & division
+
+    double computingPower;
+}
