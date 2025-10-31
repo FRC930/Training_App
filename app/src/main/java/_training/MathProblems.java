bd063e924aa1782123f378cc1ba2e097f15d3294
Hunter <carlsonh2010@gmail.com>
2025-10-31 18:22:39 -0500

    Completed the math problems part of training

--- app/src/main/java/_training/MathProblems.java
+++ app/src/main/java/_training/MathProblems.java
@@ -2,6 +2,100 @@ package _training;
 
 public class MathProblems {
     public static void main(String[] args) {
-        
+        int firstNum = 1;
+        int secondNum = 2;
+
+        int finalNum = firstNum + secondNum;
+        System.out.println(finalNum);
+
+        float firstFloat = 1.2f;
+        float secondFloat = 2.3f;
+
+        float finalFloat = firstFloat + secondFloat;
+        System.out.println(finalFloat);
+
+        double firstDouble = 1.3;
+        double secondDouble = 2.4;
+
+        double finalDouble = firstDouble + secondDouble;
+        System.out.println(finalDouble);
+
+        int thirdNum = 3;
+        int fourthNum = 4;
+
+        int numFinal = thirdNum + fourthNum;
+        System.out.println(numFinal);
+
+        int fifthNum = 2;
+        int sixthNum = 1;
+
+        int secondFinalNum = fifthNum - sixthNum;
+        System.out.println(secondFinalNum);
+
+        float thirdFloat = 2.3f;
+        float fourthFloat = 1.2f;
+
+        float secondFinalFloat = thirdFloat - fourthFloat;
+        System.out.println(secondFinalFloat);
+
+        double thirdDouble = 2.4;
+        double fourthDouble = 1.3;
+
+        double secondFinalDouble = thirdDouble - fourthDouble;
+        System.out.println(secondFinalDouble);
+
+        int seventhNum = 4;
+        int eigthNum = 3;
+
+        int thirdNumFinal = seventhNum - eigthNum;
+        System.out.println(thirdNumFinal);
+
+        int ninethNum = 2;
+        int tenthNum = 3;
+
+        int fourthFinalNum = ninethNum * tenthNum;
+        System.out.println(fourthFinalNum);
+
+        float fifthFloat = 1.2f;
+        float sixthFloat = 2.3f;
+
+        float thirdFinalFloat = fifthFloat * sixthFloat;
+        System.out.println(thirdFinalFloat);
+
+        double fifthDouble = 1.3;
+        double sixthDouble = 2.4;
+
+        double thirdFinalDouble = fifthDouble * sixthDouble;
+        System.out.println(thirdFinalDouble);
+
+        int twelthNum = 3;
+        int elevenththNum = 4;
+
+        int fourthNumFinal = twelthNum * elevenththNum;
+        System.out.println(fourthNumFinal);
+
+        int fifteenthNum = 2;
+        int sixteenthNum = 3;
+
+        int fifthFinalNum = fifteenthNum / sixteenthNum;
+        System.out.println(fifthFinalNum);
+
+        float seventhFloat = 1.2f;
+        float eigthFloat = 2.3f;
+
+        float fourthFinalFloat = seventhFloat / eigthFloat;
+        System.out.println(fourthFinalFloat);
+
+        double seventhDouble = 1.3;
+        double eigthDouble = 2.4;
+
+        double fourthFinalDouble = seventhDouble / eigthDouble;
+        System.out.println(fourthFinalDouble);
+
+        int thirteenthNum = 4;
+        int fourteenthNum = 2;
+
+        int fifthNumFinal = thirteenthNum / fourteenthNum;
+        System.out.println(fifthNumFinal);
     }
 }
