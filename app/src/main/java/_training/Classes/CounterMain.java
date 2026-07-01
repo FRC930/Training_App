f9f40c42f2ec1e6b4079cbd8fe98f21a5266b751
Luke-2029 <luke.wroblewski99@gmail.com>
2026-06-30 20:28:13 -0500

    Add instructions for CounterMain

--- app/src/main/java/_training/Classes/CounterMain.java
+++ app/src/main/java/_training/Classes/CounterMain.java
@@ -4,6 +4,7 @@ public class CounterMain {
     public static void main(String[] args) {
         Counter score = new Counter();
 
+        // Fix this line! (make score increment)
         score
     }
 }
