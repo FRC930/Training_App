218d5c91914f04a68e66cb54cdaa456ee355cc3b
chasereinders-dot <chase.reinders@gmail.com>
2026-07-21 20:50:53 -0500

    worked on variables

--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -4,9 +4,11 @@ public class Variables {
     public static void main(String[] args) {
         int myNum = 1;
         int myOtherNum = 2;
-
+        int myOtherOtherNum = 7;
         // Get the sum of the two numbers
         int myFinalNum = myNum + myOtherNum;
+        int myFinalFinalFinalFinalNum = myOtherOtherNum * myFinalNum;
         System.out.println(myFinalNum);
+        System.out.println(myFinalFinalFinalFinalNum);
     }
 }
