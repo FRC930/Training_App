340b3a31c3894f083bdf73e09f475fe7d3ca5774
Xx-Ravenal-xX <evan.n.geist@gmail.com>
2026-08-13 20:52:42 -0500

    got to robots.java

--- app/src/main/java/_training/Robots.java
+++ app/src/main/java/_training/Robots.java
@@ -11,5 +11,20 @@ public class Robots {
         int robotSpeed = 20; // Set the robot speed
         driveRobot("forward", robotSpeed); // Drive the robot forward
         shootRobot("left"); // Shoot the robot to the left
+
+        public class Robots1 {
+    static void driveRobot1(String direction1, int speed){
+        System.out.println("Robot is driving "+direction1);
+    }
+    static void shootRobot1(String direction1){
+        System.out.println("Robot is shooting to the "+direction);
+    }
+    public static void main1(String[] args1){
+        int robotSpeed1 = 2099; // Set the robot speed
+        driveRobot1("backward", robotSpeed1); // Drive the robot forward
+        shootRobot1("right"); // Shoot the robot to the left
+    }
+}
+
     }
 }
--- app/src/main/java/_training/Variables.java
+++ app/src/main/java/_training/Variables.java
@@ -4,9 +4,48 @@ public class Variables {
     public static void main(String[] args) {
         int myNum = 1;
         int myOtherNum = 2;
+        int myOtherOtherNum = 3;
 
         // Get the sum of the two numbers
-        int myFinalNum = myNum + myOtherNum;
+        int myFinalNum = (myNum + myOtherNum) * myOtherOtherNum;
         System.out.println(myFinalNum);
+
+        String myText = "Hello ";
+        String myOtherText = "Again";
+        String myFinalText = myText + myOtherText;
+
+        System.out.println(myFinalText);
+
+        float myDecimal = 2.5f;
+        float mySecondDecimal = 5.0f;
+
+        float myLastDecimal = myDecimal / mySecondDecimal;
+
+        System.out.println(myLastDecimal);
+
+        double myDoubleDecimal = 6.7;
+        double myOtherDouble = 4.1;
+        
+        double myFinalDouble = myDoubleDecimal / myOtherDouble;
+
+         System.out.println(myFinalDouble);
+
+         boolean ateBreakfast = true;
+         boolean ateLunch = true;
+         boolean ateDinner = false;
+
+         System.out.println(ateBreakfast && ateLunch);
+         System.out.println(ateLunch && ateDinner);
+
+        boolean brushedTeeth = true;
+        boolean tookMints = false;
+        boolean didHomework = false;
+        boolean preparedForRobotics = true;
+         if ( (brushedTeeth || tookMints) && didHomework && preparedForRobotics) {
+    System.out.println("I can go to robotics tonight!");
+} else {
+    System.out.println("I can't go to robotics yet.");
+}
+         
     }
 }
